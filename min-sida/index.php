54ed1d7db97bb9d71c8a2a40bc9b3c9a2ddd66b9
ra222hdRasha <ra222hd@lnu.se>
2015-02-16 17:36:41 +0100

    SKapade en första index-fil

---
+++ min-sida/index.php
@@ -0,0 +1,25 @@
+<?php #Script - index.php
+ 
+ $page_title = 'Welcome to this Site';
+ include ('includes/header.html');
+ ?>
+ <h1>OM MIG SJÄLV</h1>
+ 
+ <p>Jag är intresserad av att få användning för och kunna bidra med mina kunskaper i en såväl professionell som intressant organisation . Min önskan är att få arbeta med uppgifter relaterade till systemutveckling där användaren sätts i fokus med syfte att uppnå största möjliga affärsnytta för samtliga parter.
+ 
+Jag blev klar med min högskoleingenjör i  Elektronik med inriktning mot
+Telekommunikation. Utbildningen gör mig redo för att jobba med underhåll, konfigurering och utveckling inom de flesta områden av telekomindustrin. 
+
+Eftersom min utbildning har givit mig att de verktyg som behövs för att kunna möta nya utmaningar  som IT- Datateknik hos Elektronik. Jag är färdig att ha förmåga med helhetssyn självständigt och kreativt identifiera , formulera och hantera frågeställningar och analysera och utvärdera olika tekniska lösningar.</p>
+ <img src="images/girl.gif" alt="Bild på Rasha Altoumaimi">
+<p>Jag är en person som verkligen  uppskattar nya utmaningar och tar mig an dem med ett stort initiativ  och engagemang. Som kollega är jag bra på att motivera andra, inte minst i stressiga situationer och anser att god humor kan lätta upp de flesta situationer.  I mitt jobb strävar jag efter att få en bra överblick på mina arbetsuppgifter och löser dem med fokus på resultateffektivitet.
+
+För att kunna jobba effektivt så krävs det teamwork, något som jag verkligen ha lärt mig genom min studier i högskolan. Jag tycker att en god relation klasskamrater eller kollegor emellan är grunden för att arbeta ska flyta på så smidigt som möjligt.</p>
+
+<p>Hösten 2013 tog jag studenten vid Halmstad högskolan i Datornätverk där jag läste CISCO:s CCNA. Inom min utbildning har jag läst mig hur nätverk  bygger och konfigurera router och switchar med tillämpar protokoll och tekniker som an -vänds av olika typer av WAN (Wide Area Network).
+
+Våren 2014 tog jag studenten vid Linneuniversitetet i Data och systemvetenskap. Under högskoleutbildning har jag läst kurser i såväl programmering som systemutveckling och människa -& datorinteraktion och vet därmed vilken stor roll funktionalitet samt användbarhet spelar i olika typer av datasystem.
+</p>
+<?php
+ include('includes/footer.html');
+ ?>
